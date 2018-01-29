151886ceefd84884168f1f2210e6a3e97815c59d
Jolene <joh112@eng.ucsd.edu>
2018-01-28 22:55:59 -0800

    preprocessing

---
+++ DSB/docker/dsbpreproc/preprocess.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p /dsb3/preproc/images
+mkdir -p /dsb3/preproc/csv
+
+# run preprocessing
+python step0_preprocess.py
---
+++ DSB/docker/dsbpreproc/settings.py
@@ -0,0 +1,22 @@
+BASE_DIR = "./data/"
+BASE_PREPROCESS_DIR = "./preproc/"
+BASE_PREPROCESSEDCSV_DIR = BASE_PREPROCESS_DIR + "csv/"
+BASE_PREPROCESSEDIMAGES_DIR = BASE_PREPROCESS_DIR + "images/"
+BASE_TRAIN_SEGMENT_DIR = BASE_PREPROCESS_DIR + "data_segmenter_trainset/"
+PATIENT_PRED_DIR = BASE_PREPROCESS_DIR + "data_patient_predictions/"
+
+# Quick mode does away with training in different folds.
+# It does overfit a little in the calibration and submission step.
+# However it still scores ~0.010552 on the private LB which is enough for the 3rd place
+# The advantages is that it takes only 4-5 hours to train and 1 hour to predict.
+QUICK_MODE = True
+
+MODEL_NAME = "model_quick" if QUICK_MODE else "model_full"
+TRAIN_EPOCHS = 20 if QUICK_MODE else 30
+FOLD_COUNT = 6
+TARGET_SIZE = 256
+CROP_INDENT_X = 32
+TARGET_CROP = 184
+CROP_INDENT_Y = 32 - ((TARGET_CROP - 160) / 2)
+CROP_SIZE = 16
+
