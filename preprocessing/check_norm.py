78f75d922626c7ec37071d519191b38aa55e3b54
Jolene <joh112@eng.ucsd.edu>
2018-04-15 15:58:37 -0700

    checking norm files

---
+++ preprocessing/check_norm.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import sys
+
+def checking_npy(filename):
+    t = np.load(filename)
+    print t.size
+    print len(t)
+    print np.sum(t)
+    print t
+
+
+if __name__ == "__main__":
+    argv = sys.argv 
+    print argv
+    filename = argv[1]
+    checking_npy(filename)
