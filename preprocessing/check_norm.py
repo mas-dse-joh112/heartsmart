b4ed562b5bd663185ffa559a64899db820db7f8b
Jolene <joh112@eng.ucsd.edu>
2018-05-03 21:11:45 -0700

    convert to py3

--- preprocessing/check_norm.py
+++ preprocessing/check_norm.py
@@ -4,14 +4,14 @@ import sys
 
 def checking_npy(filename):
     t = np.load(filename)
-    print t.size
-    print len(t)
-    print np.sum(t)
-    print t
+    print (t.size)
+    print (len(t))
+    print (np.sum(t))
+    print (t)
 
 
 if __name__ == "__main__":
     argv = sys.argv 
-    print argv
+    print (argv)
     filename = argv[1]
     checking_npy(filename)
