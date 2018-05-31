07f5b775cb022c7bb6488bcf5f5549e6edfd3fd9
Jolene <joh112@eng.ucsd.edu>
2018-05-31 01:20:47 -0700

    integrate code from unet-LWv5

--- Final/unet_model/unet_predict.py
+++ Final/unet_model/unet_predict.py
@@ -18,35 +18,29 @@ from keras.models import *
 from keras.layers import Input, concatenate, merge, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Cropping2D
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from keras.losses import binary_crossentropy
-import keras.backend as K
 from keras import backend as keras
-#from helper_functions  import *
-
-from helper_utilities import *
-#from helper_plotutilities import *
-from loss_functions  import *
-
-#from unet_model_batchnormal  import *
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+#from keras.losses import binary_crossentropy
+import keras.backend as K
+#from loss_functions  import *
+from helper_utilities  import *
 from unet_model import *
 
-#Fix the random seeds for numpy (this is for Keras) and for tensorflow backend to reduce the run-to-run variance
-from numpy.random import seed
-seed(12345)
-from tensorflow import set_random_seed
-set_random_seed(12345)
-
-import tensorflow as tf
+from modelmgpu import ModelMGPU
 
 GPU_CLUSTER = "0,1,2,3,4,5,6,7" # set in config file, override later
 os.environ["CUDA_VISIBLE_DEVICES"] = GPU_CLUSTER
 GPUs = None
 
-from modelmgpu import ModelMGPU
+#Fix the random seeds for numpy (this is for Keras) and for tensorflow backend to reduce the run-to-run variance
+from numpy.random import seed
+seed(100)
+from tensorflow import set_random_seed
+set_random_seed(200)
+
+import tensorflow as tf
 
 import time
-
 start = time.time()
 print("START:", start)
 
@@ -55,66 +49,65 @@ config.gpu_options.allow_growth = True
 
 print("\nSuccessfully imported packages!!!\n")
 
-#################
-# Method load pre-trained weights to unet-model and run predictions 
-#
-##########################
 
-
-def predict_with_pretrained_weights(model_name, nGPU, model_file, image_size, test_image_file, test_label_file = "none", augmentation = False, contrast_normalize = False):
+def predict_with_pretrained_weights(args):
     """
+    Load the pretrained weights with the model file to predict
     
-
     Args:
-      model_name:  unique model name for identificatio
-      nGPU: number of GPU devices using
-      model_file: model file for predicting
-      image_size: uniform image size
-      test_image_file: image files in 4d numpy array for testing
-      test_label_file:  (Default value = "none"), label files or none
-      augmentation:  (Default value = False)
-      contrast_normalize:  (Default value = False)
+      args: parameters passed in from the prediction config file
 
-    Returns: U-net model instance
+    Returns: none
 
     """
-    img_size_list = [176, 256]
-
-    if image_size not in img_size_list:
-        print ("image size %d is not supported"%image_size)
-        return 
-    
-    size_str = str(image_size)
-    
-    test_data = {}
-    test_data["images"] = test_image_file
-    test_data["labels"] = test_label_file
-
-    model_file = model_file
-
-    print('-'*30)
-    print ("Get Test images and labels...")
-
-    if test_label_file == "none": 
-        #ts = load_images(test_image_file, normalize= True, contrast_normalize = True)
-        ts = load_images(test_image_file, normalize= True, contrast_normalize = contrast_normalize)
-        tl = "none"
-    else :
-        ts , tl= load_images_and_labels(test_data, normalize= True, contrast_normalize = contrast_normalize)
-
-    print('-'*30)
     print ("Creating U-net model...")
-    myunet = myUnet(model_name = model_name, nGPU = nGPU, image_size = image_size)
+    myunet = myUnet(model_name = args.model_name, nGPU = args.GPUs, image_size = args.image_size, batch_norm = args.batch_normalization, dropout = args.dropout)
     print('-'*30)
-    print ("Loading the pre-trained weights...")
-    myunet.load_pretrained_weights(model_file)
+    myunet.model_file = args.hdf5_path
+    print ("Loading the pre-trained weights...", myunet.model_file)
+    myunet.load_pretrained_weights(myunet.model_file)
     print('-'*30)
+ 
+    myunet.model_path = "{0}_models".format(args.predict_path) # where one_count.json files are
+    myunet.volume_path = args.volume_path
+    myunet.source_type = args.source_type # train, test, validate, or roi
+    myunet.source = args.source # train, test, validate
+    myunet.data_source = "unet_model_{0}".format(myunet.source_type)
+    myunet.file_source = args.file_source
+    myunet.test_source_path = "/masvol/output/{0}/norm/{1}/{2}".format(myunet.file_source, args.method, args.Type)
+    myunet.method = args.method
+    myunet.image_size = args.image_size
+    myunet.predict_path = "{0}_predict".format(args.predict_path) # where ts_norm, pred_round, and predictions.json reside
+    myunet.augmentation = args.augmentation
+    myunet.batch_norm = args.batch_normalization
+    myunet.dropout = args.dropout
+    myunet.contrast_normalize = args.contrast_normalization
 
-    print('Run predictions...')
-    myunet.predict(test_image_array = ts, test_label_array = tl, augmentation=augmentation)
-    print('-'*30)
+    inputpath = "/masvol/data/{0}/{1}/*".format(myunet.file_source, myunet.source)
+    pcount = 0
+    print (inputpath)
 
-    return myunet
+    for i in glob.glob(inputpath):
+        nodes = i.split('/')
+        myunet.patient = nodes[-1]
+        print ('|'+myunet.patient+'|')
+        # test_image_file: image files in 4d numpy array for testing, image_4d_file
+        myunet.image_4d_file = "{0}/{1}/data/{2}_{3}_{4}_train.npy".format(myunet.test_source_path, myunet.data_source, myunet.file_source, myunet.patient, myunet.image_size)
+        print ('4d',myunet.image_4d_file)
+        myunet.image_source_file = "{0}/{1}/data/{2}_{3}_image_path.txt".format(myunet.test_source_path, myunet.data_source, myunet.file_source, myunet.patient)
+        print ('sf',myunet.image_source_file)
+        myunet.image_one_file = "{0}/{1}/{2}/{3}_{4}_{5}_one_count.json".format(myunet.test_source_path, myunet.data_source, myunet.model_path, myunet.file_source, myunet.patient, myunet.image_size)
+        # test_labels:  (Default value = "none"), label files or none
+        myunet.test_labels = "none"
+        #print ('io',myunet.image_one_file)
+        myunet.do_predict()
+        myunet.fourD_add1() 
+        myunet.get_ones()
+        myunet.dump_and_sort()
+
+        pcount += 1
+        #if pcount > 1:
+        #    break
 
 
 if __name__ == "__main__":
@@ -123,42 +116,40 @@ if __name__ == "__main__":
 
     myconfig = sys.argv[1]
 
+    if myconfig.endswith('.py'):
+        myconfig = myconfig.replace('.py','')
+
     args = __import__(myconfig)
 
     img_size_list = [176, 256]
 
-    arg_list = ['model_name','image_size','weights_file','image_file','label_file','save_folder','augmentation','contrast_normalization']
+    # fit and predict for all patients with source_type
+    # model_name:  unique model name for identification
+    # GPUs: number of GPU devices using
+    # model_file: model file for predicting, hdf5_path
+    # image_size: uniform image size
+    # augmentation:  (Default value = False)
+    # contrast_normalization: (Default value = False)
+    arg_list = ['model_name','image_size','source','source_type','method','Type','hdf5_path','predict_path','volume_path','augmentation','contrast_normalization','batch_normalization','GPU_CLUSTER','GPUs','per_process_gpu_memory_fraction','file_source','dropout']
+
     dir_args = dir(args)
 
     for x in arg_list:
         if x not in dir_args:
             print ("insufficient arguments ")
-            print (" enter model_name, image_size, model_file (weights file), image_file, label_file(enter none if no labels), save_folder(to save predictions), augmentation (True or False), contrast_normalization (True or False) in the config file")
+            print ("enter {0} in the config file".format(",".join(arg_list)))
             sys.exit() 
 
-    image_size = args.image_size
-
-    if image_size not in img_size_list:
-        print ("image size %d is not supported"%image_size)
+    if args.image_size not in img_size_list:
+        print ("image size %d is not supported"%args.image_size)
         sys.exit()
 
     os.environ["CUDA_VISIBLE_DEVICES"] = args.GPU_CLUSTER
-    GPUs = len(args.GPU_CLUSTER.split(','))
+    args.GPUs = args.GPUs
     config.gpu_options.per_process_gpu_memory_fraction = args.per_process_gpu_memory_fraction
     session = tf.Session(config=config)
 
-    model_name = args.model_name
-    image_size = args.image_size
-    weights_file = args.weights_file
-    image_file = args.image_file
-    label_file = args.label_file
-    save_folder = args.save_folder
-    augment = args.augmentation
-    contrast_norm = args.contrast_normalization
+    predict_with_pretrained_weights(args)
 
-    mymodel = predict_with_pretrained_weights(model_name=model_name,nGPU=0,model_file=weights_file,image_size=image_size, \
-           test_image_file=image_file, test_label_file=label_file, augmentation=augment, contrast_normalize=contrast_norm)
-    
-    pred_file =  save_folder + model_name + "_predictions.npy"
-    print ("Saving predictions", pred_file)
-    np.save(pred_file, mymodel.predictions)
+    end = time.time()
+    print ("END:", end - start)
