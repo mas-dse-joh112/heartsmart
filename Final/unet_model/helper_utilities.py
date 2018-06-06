bee5cc73410a0d7535d23a0e4783af3c40e072de
Jolene <joh112@eng.ucsd.edu>
2018-06-06 01:33:25 -0700

    ensamble

--- Final/unet_model/helper_utilities.py
+++ Final/unet_model/helper_utilities.py
@@ -15,6 +15,11 @@ from sklearn.metrics import log_loss
 from sklearn.metrics import auc, roc_curve, accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 from collections import OrderedDict
 from helpers_dicom import DicomWrapper as dicomwrapper
+import skimage
+from skimage import measure,feature
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
 
 perf_keys = ["samples", "logloss", "weighted_logloss","accuracy", "weighted_accuracy", "dice_coef", "precision","recall", \
              "f1_score", "true_positive", "false_positive","true_negative","false_negative", "zero_contour_labels", \
@@ -24,12 +29,20 @@ perf_keys_ext=["tr_model_name","tr_nGPUs", "tr_loss_fn","tr_dropout", "tr_optimi
                "tr_size","tr_contrast_norm","tr_augmentation","tr_augment_count","tr_augment_shift_h", "tr_augment_shift_w", \
                "tr_augment_rotation","tr_augment_zoom","eval_loss","eval_dice_coeff","eval_binary_accuracy","logloss", \
                "accuracy","weighted_logloss", "weighted_accuracy","precision","recall","f1_score", "true_positive", \
-             "true_negative", "false_positive", "false_negative"]
+               "true_negative", "false_positive", "false_negative"]
+
+perf_keys_ext2=["eval_loss","eval_dice_coeff","eval_binary_accuracy","logloss", \
+                "accuracy","precision","recall","f1_score", "true_positive", \
+                "true_negative", "false_positive", "false_negative"]
+
 def get_perf_keys():
     return perf_keys
 
 def get_perf_keys_ext():
     return perf_keys_ext
+
+def get_perf_keys_ext2():
+    return perf_keys_ext2
 
 def load_images_and_labels(data, normalize= True, zscore_normalize = False, contrast_normalize= False, cliplimit=2, tilesize=8 ):
     """Function to load images and labels from .npy file into a 4d numpy array
@@ -265,7 +278,7 @@ def load_images(imgfile, normalize= True, zscore_normalize = False, contrast_nor
         images2 = (images - x_min)/(x_max-x_min)
         print("shape, max, min, mean of original image set:", images.shape, images.max(), images.min(), images.mean())
         print("shape, max, min, mean after normalization  :", images2.shape, images2.max(), images2.min(), images2.mean())
-    elif (zscore_noramlize == True):
+    elif (zscore_normalize == True):
         images = im.astype('float32')
         ##Normalize the pixel values, (between 0..1)
         x_mean = images.mean(axis=(1, 2), keepdims=True)
@@ -431,6 +444,11 @@ def get_performance_statistics(y_true_f, y_pred_f):
     perf["accuracy"] = math.exp(-score)
     perf["weighted_accuracy"] = math.exp(-score2)
 
+    smooth = 1.
+    intersection = np.sum(y_true * y_pred)
+    dice_coef = (2. * intersection + smooth) / (np.sum(y_true) + np.sum(y_pred) + smooth)
+    perf["dice_coef"] = dice_coef
+    
     y_pred = np.round(y_pred)
     perf["precision"] = precision_score(y_true, y_pred, average="binary")
     perf["recall"] = recall_score(y_true, y_pred, average="binary")
@@ -776,6 +794,328 @@ def combine_acdc_sunnybrook_data(unet_train_dir, acdc_source, sb_source, image_s
     np.save(tst_img_file, combined_test_img)
     np.save(tst_lbl_file, combined_test_lbl)
     
+def remove_contour(predictions):
+    """ 
+    At times the model predicts extra more than one contour. Since there is only one LV, we remove           extra predicted contours.
+    The pixel density id highest in LV. The numpy array per patient is summed across all the frames.
+    The coordinates for max are taken. The contour containing this point is only retained. In case           in any frame the point is not contained in any frame; no contours are kept.
+    Args:
+      self: The current patient data in processing
+    """
+
+    t=predictions
+    found_dic={}
+    count=0
+    #print(t.shape[0])
+    x=0
+    for i in range(t.shape[0]):
+        x=x+t[i,:,:,0]
+
+    print(np.max(x))
+    tgt=np.where(x==np.max(x))
+    #print('tgt',tgt)
+    x1=max(tgt[0])
+    y1=max(tgt[1])
+    #print('xy',x1,y1)
+    point = Point(x1, y1)    
+
+    for t_im1 in predictions:
+        found_dic={} 
+        t_im1 = t_im1[:, :, 0]        
+        dict_shape={}
+        cntrs=skimage.measure.find_contours(t_im1,0.1)  
+        found = []
+
+        if len(cntrs)>1:
+            try:
+                for i in range(len(cntrs)):
+                    polygon = Polygon(cntrs[i])  
+                    if polygon.contains(point):
+                        found_dic[i]=1  
+                    else:
+                        found.append(i)
+            except ValueError:            
+                print(len(cntrs[i]),cntrs[i])            
+
+        for j in found:
+            dict_shape[j]=cntrs[j].shape[0]
+
+            for k in dict_shape:
+               x1=math.trunc(np.min(cntrs[k],axis=0)[0])
+               y1=math.trunc(np.min(cntrs[k],axis=0)[1])
+               x2=math.trunc(np.max(cntrs[k],axis=0)[0])
+               y2=math.trunc(np.max(cntrs[k],axis=0)[1])
+               t_im1[x1:x2+1, y1:y2+1]=0
+    
+    return predictions
+
+def get_ones(predictions, image_source_file):
+    """ Count up the 1s in predictions """
+
+    print ('l', len(predictions))
+    sourcefiles = []
+    sourcedict = dict()
+
+    with open(image_source_file, 'r') as sourceinput:
+        for i in sourceinput:
+            sourcefiles = i.strip().split(',')
+
+    print ('SF',len(sourcefiles))
+
+    for i in sourcefiles:
+        sourcedict[i] = {'ones':0} # init, may not have prediction
+
+    for i in range(len(predictions)):
+        zcount = np.count_nonzero(predictions[i])
+        sourcedict.update({sourcefiles[i]: {'ones':zcount}}) # save ones count for now
+
+    return sourcedict
+
+def remove_ctrsAll(arr):
+    t=arr
+    found_dic={}
+    count=0
+    #print(t.shape[0])
+    x=0
+    for i in range(t.shape[0]):
+        #print(np.max(t[i,:,:,0]))
+        x=x+t[i,:,:,0]
+    print(np.max(x))
+    tgt=np.where(x==np.max(x))
+    #print('tgt',tgt)
+    x1=max(tgt[0])
+    y1=max(tgt[1])
+    print('xy',x1,y1)
+    point = Point(x1, y1)    
+    print(x[x1,y1])
+    icount = 0
+    n=0
+    for t_im1 in arr:
+        found_dic={} 
+        t_im1 = t_im1[:, :, 0]        
+        icount += 1
+        #print('count',count)
+        #print(t_im1.shape)
+        dict_shape={}
+        remove_dict={}
+        sorted_keys=[]
+        fill_found=[]
+        cntrs=skimage.measure.find_contours(t_im1,0.1)  
+        for i in range(len(cntrs)):
+            #print ('contour:'+str(i)+'  shape[0]:'+str(cntrs[i].shape[0]))
+            dict_shape[i]=cntrs[i].shape[0]  
+        sorted_keys = sorted(dict_shape, key=dict_shape.get,reverse=True) 
+
+        #print('dict_shape',dict_shape)
+        #print('sorted_keys',sorted_keys)
+        #if icount==15:
+            #break
+        found = []
+        in_loop=0
+        #print('ctrs count',len(cntrs))
+        if len(cntrs)>1:
+            #print('# of contours identified:::'+str(len(cntrs)), icount)
+            #plt.subplot(121),plt.imshow(t_im1,)
+            try:
+
+                for i in sorted_keys:
+                    #print(type(cntrs[i]),cntrs[i].shape)
+                    #print ('cntrs',cntrs[i])
+                    in_loop+=1
+                    if len(found_dic)==0:
+                        polygon = Polygon(cntrs[i])  
+                        if polygon.contains(point):
+                            found_dic[i]=1
+                            for j in range(in_loop,len(sorted_keys)):
+                                polygon1 = Polygon(cntrs[sorted_keys[j]]) 
+                                if polygon.contains(polygon1):
+                                    print('Yes the polygon contains sub polygon')
+                                    fill_found.append(sorted_keys[j])
+                                else:
+                                    found.append(sorted_keys[j])
+                            print('Found after polygon contain',found)        
+                        #found=list(found_dic.keys())   
+
+                        else:
+                            found.append(i)  
+                            print('Since polygon did not contain, found is ',found)
+                    else:
+                        continue 
+                        print('since already found a contour, found is', found)
+            except ValueError:            
+                print(len(cntrs[i]),cntrs[i]) 
+            print('found',found)    
+        count=count+1    
+        
+        remove_dict={}
+
+        for k in fill_found:
+            print(k)
+            print ('filling contour '+str(k))
+            x1=math.trunc(np.min(cntrs[k],axis=0)[0])
+            y1=math.trunc(np.min(cntrs[k],axis=0)[1])
+            x2=math.trunc(np.max(cntrs[k],axis=0)[0])
+            y2=math.trunc(np.max(cntrs[k],axis=0)[1])
+            t_im1[x1:x2+1, y1:y2+1]=1   
+        for k in found:
+            print(k)
+            print ('deleting contour '+str(k))
+            x1=math.trunc(np.min(cntrs[k],axis=0)[0])
+            y1=math.trunc(np.min(cntrs[k],axis=0)[1])
+            x2=math.trunc(np.max(cntrs[k],axis=0)[0])
+            y2=math.trunc(np.max(cntrs[k],axis=0)[1])
+            t_im1[x1:x2+1, y1:y2+1]=0
+    return arr        
+
+def make_dir(dirpath):
+    dirname = os.path.dirname(dirpath)
+    print ('dirname', dirname)
+
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+def do_ensamble_vote(models, base_dir, out_dir, patient):
+    print ('models', models)
+    model_num = len(models)
+    denom = model_num*1.0
+    vote_threshold = denom/2 + 1
+    image_list=[]
+        
+    for model in models:
+        modelpath = "{0}/{1}_predict/".format(base_dir, model)
+
+        for files in glob.glob(modelpath+'/dsb_'+patient+'*predictions.npy'):
+            fpatient = files.split('/')[-1].split('_')[1]
+            
+            if int(patient) == int(fpatient):                    
+                image_list.append(files)
+            
+    if len(image_list) != int(denom):
+        print ('error: not equal')
+        return np.array([])
+    
+    if model_num == 0:
+        return np.array([])
+
+    ensamble_pred = None
+
+    for i in range(len(image_list)):
+        pred = np.load(image_list[i])
+        pred = np.round(pred)
+
+        if i == 0:
+            ensamble_pred = pred
+        else:
+            ensamble_pred = np.add(ensamble_pred, pred)
+        
+    ensamble_pred = ensamble_pred//vote_threshold
+    ensamble_round_pred = "{0}/{1}_ensamble_round_predict.npy".format(out_dir, patient)
+    make_dir(ensamble_round_pred)
+    np.save(ensamble_round_pred, ensamble_pred)
+    return ensamble_pred
+
+def do_ensamble_average(models, base_dir, out_dir, patient):
+    print ('models', models)
+    model_num = len(models)
+    denom = model_num*1.0
+    image_list=[]
+        
+    for model in models:
+        modelpath = "{0}/{1}_predict/".format(base_dir, model)
+        print ('mp', modelpath)
+
+        for files in glob.glob(modelpath+'/dsb_'+patient+'*predictions.npy'):
+            fpatient = files.split('/')[-1].split('_')[1]
+            
+            if int(patient) == int(fpatient):                    
+                image_list.append(files)
+            
+    if len(image_list) != int(denom):
+        print ('error: not equal')
+        return np.array([])
+    
+    if model_num == 0:
+        return np.array([])
+
+    ensamble_pred = None
+
+    for i in range(len(image_list)):
+        if i == 0:
+            ensamble_pred = np.load(image_list[i])
+        else:
+            ensamble_pred = np.add(ensamble_pred, np.load(image_list[i]))
+        
+    ensamble_predict = "{0}/{1}_ensamble_predict.npy".format(out_dir, patient)
+    make_dir(ensamble_predict)
+    np.save(ensamble_predict, ensamble_pred)
+
+    ensamble_round = np.round(ensamble_pred)
+    ensamble_round_pred = "{0}/{1}_ensamble_round_predict.npy".format(out_dir, patient)
+    make_dir(ensamble_round_pred)
+    np.save(ensamble_round_pred, ensamble_round)
+    return ensamble_round
+
+def post_processing(base_dir, source, input_dir, volume_dir, ensampath, patient, image_size, predictions):
+    #base_dir = '/masvol/output/dsb/norm/1/3/unet_model_'
+    #source = 'train'
+    #input_dir = '/masvol/data/dsb'
+    #ensampath = '/masvol/output/dsb/norm/1/3/ensamble/top_systolic'
+    #                                                 'top_diastolic'
+    #volume_dir = '/masvol/output/dsb/volume/1/3path'
+
+    image_source_file = "{0}{1}/data/dsb_{2}_image_path.txt".format(base_dir, source, patient)
+    sourcedict = get_ones(predictions, image_source_file)
+
+    pred_file_CR = "{0}/dsb_{1}_{2}_CR4d_predictions_cleaned.npy".format(ensampath, patient, image_size)
+    np.save(pred_file_CR, predictions)
+
+    image_one_file = "{0}/dsb_{1}_{2}_one_count.json".format(ensampath, patient, image_size)
+
+    with open(image_one_file, 'w') as output:
+        output.write("{0}\n".format(json.dumps(sourcedict)))
+
+    origpath = '{0}/{1}/{2}/study/'.format(input_dir, source, patient)
+    newpath = '{0}/{1}_{2}_{3}.json'.format(volume_dir,source,patient,image_size)
+
+    make_dir(newpath)
+    dump_and_sort(image_one_file, origpath, newpath)
+
+def do_ensamble(args):
+    input_dir = args.input_dir
+    base_dir = args.base_dir
+    sources = args.sources
+    output_dir = args.output_dir
+    diastolic_models = args.diastolic_models
+    systolic_models = args.systolic_models
+    systolic_path = args.systolic_path
+    diastolic_path = args.diastolic_path
+    volume_dir = args.volume_dir
+    ensamble_type = args.ensamble_type
+    image_size = args.image_size
+
+    for source in sources:
+        for patient in glob.glob("{0}/{1}/*".format(input_dir,source)):
+            nodes = patient.split('/')
+            patient = nodes[-1]
+
+            dia_ensamble_round = None
+            sys_ensamble_round = None
+
+            if ensamble_type == 1:
+                print ('voting')
+                dia_ensamble_round = do_ensamble_vote(diastolic_models, base_dir+source, output_dir+diastolic_path, patient)
+                sys_ensamble_round = do_ensamble_vote(systolic_models, base_dir+source, output_dir+systolic_path, patient)
+            else:
+                dia_ensamble_round = do_ensamble_average(diastolic_models, base_dir+source, output_dir+diastolic_path, patient)
+                sys_ensamble_round = do_ensamble_average(systolic_models, base_dir+source, output_dir+systolic_path, patient)
+
+            print ('SYS', sys_ensamble_round.shape, dia_ensamble_round.shape)
+            if dia_ensamble_round.shape[0] > 0:
+                post_processing(base_dir, source, input_dir, volume_dir, output_dir+diastolic_path, patient, image_size, dia_ensamble_round)
+
+            if sys_ensamble_round.shape[0] > 0:
+                post_processing(base_dir, source, input_dir, volume_dir, output_dir+systolic_path, patient, image_size, sys_ensamble_round)
+
 
 if __name__ == "__main__":
     dummy = 1
